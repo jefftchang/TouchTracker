d14514422ec58c0cd53a2374bc99d9e17451d65c
Jeff Chang <jeffchang138@gmail.com>
2012-10-30 10:23:06 -0700

    Initial Commit

---
+++ TouchTracker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TouchTracker
+//
+//  Created by Jeff Chang on 10/30/12.
+//  Copyright (c) 2012 Jeff Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
